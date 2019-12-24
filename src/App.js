87c71a19a9ec6576528ec12599ca965fd756fb96
psx95 <pranav.ps95@hotmail.com>
2019-12-24 02:35:43 -0500

    feat(welcome): adds a conditional component to show logged in user name

--- src/App.js
+++ src/App.js
@@ -4,19 +4,25 @@
 // will be overwritten by the bootstrap.css
 import React, { Component } from 'react';
 import Home from './Home'
+import Welcome from './Welcome';
 
 class App extends Component {
 
   constructor(props) {
     super(props)
     this.state = {
-      user: 'PSX'
+      user: "Pranav"
     }
   }
 
   render() {
     return (
-      <Home user={this.state.user} />
+      <div>
+        {this.state.user && (
+          <Welcome user={this.state.user} />
+        )}
+        <Home user={this.state.user} />
+      </div>
     );
   }
 }
---
+++ src/Welcome.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class Welcome extends Component {
+    render() {
+        const { user } = this.props
+        return (
+            <div className="text-center mt-4"> {/* bootstrap class of mt-4 margin of 4 points from top */}
+                <span className="text-secondary font-weight-bold pl-1">
+                    Welcome, {user}
+                </span>
+                <a href="/" className="font-weight-bold text-primary pl-1">
+                    log out
+                </a>
+            </div>
+        )
+    }
+}
+
+export default Welcome
