329601732ec7309d691c7946f6b19ca5c5735797
psx95 <pranav.ps95@hotmail.com>
2019-12-24 01:36:22 -0500

    feat(home): updates css properties

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 // Import React
+// the import order matters, currently the bootstrap.css file is loaded here 
+// and the index.js loads the App component and so the index.css (which was imported in index.js)
+// will be overwritten by the bootstrap.css
 import React, { Component } from 'react';
 import Home from './Home'
-
-import 'bootstrap/dist/css/bootstrap.css';
 
 class App extends Component {
   render() {
