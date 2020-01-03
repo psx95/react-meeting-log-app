a746dd31df17b042a48f5bda77c75a0ef6550005
psx95 <pranav.ps95@hotmail.com>
2020-01-03 11:51:19 -0500

    feat(attendees): adds a search feature to search for attendees in list

--- src/Attendees.js
+++ src/Attendees.js
@@ -6,8 +6,10 @@ class Attendees extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            searchQuery: "",
             displayAttendees: []            
-        };
+        };    
+        this.handleChange = this.handleChange.bind(this);
     }
 
     componentDidMount() {
@@ -27,19 +29,37 @@ class Attendees extends Component {
         });
     }
 
+    handleChange(event) {
+        const itemName = event.target.name;
+        const itemValue = event.target.value;
+
+        this.setState({ [itemName]: itemValue });
+    }
+
     render() {
+        const dataFilter = item => item.attendeeName
+            .toLowerCase()
+            .match(this.state.searchQuery.toLowerCase()) && true;
+        
+        const filteredAttendees = this.state.displayAttendees.filter(dataFilter);
+
         return (
             <div className="container mt-4">
                 <div className="row justify-content-center">
                     <div className="col-md-8">
                     <h1 className="font-weight-light text-center">
                         Attendees
-                    </h1>
+                    </h1>                    
+                        <div className="card bg-light mb-4">
+                            <div className="card-body text-center">
+                                <input type="text" name="searchQuery" value={this.state.searchQuery} placeholder="Search Attendees" className="form-control" onChange={this.handleChange}/>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 {/**The userId passed below will allow us to add conditional checks like if user is admin, he may have the option to add or delete more attendees */}
                 <AttendeesList userId={this.props.userId}
-                    attendees={this.state.displayAttendees}
+                    attendees={filteredAttendees}
                     adminUser={this.props.adminUser}
                     meetingId={this.props.meetingId}/>
             </div>
